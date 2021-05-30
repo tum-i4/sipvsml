753e7f510d551d87e2fc5033c8a5ea6b13a08137
Nika <ndogh13@freeuni.edu.ge>
2021-05-31 02:54:34 +0400

    Write training results to a json file

--- sip_vs_pipeline/model_training/train_model.py
+++ sip_vs_pipeline/model_training/train_model.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import pathlib
+from datetime import datetime
 
 from sip_vs_pipeline.model_training.data_reader import SIPDataSet
 from sip_vs_pipeline.model_training.graph_sage import GraphSageSIPLocalizer
@@ -15,8 +17,43 @@ def parse_args():
         '--use_features', type=str, nargs='+', choices=['ir2vec', 'seg', 'tf_idf'],
         help='Name of the features to use', required=True
     )
+    parser.add_argument(
+        '--save_results_path', type=str, required=True,
+        help='Path to the file where training results will be stored'
+    )
     args = parser.parse_args()
     return args
+
+
+def create_model(model_name):
+    if model_name == 'graph_sage':
+        return GraphSageSIPLocalizer()
+    raise RuntimeError(f'Unknown model {model_name}')
+
+
+def write_json(training_res, training_results_path):
+    with open(training_results_path, 'w', encoding='utf-8') as out:
+        json.dump(training_res, out, ensure_ascii=False, indent=4)
+
+
+def run(labeled_bc_dir, model_name, features, training_results_path):
+    if training_results_path.exists():
+        print(f'{training_results_path} already exists, exiting...')
+        return
+
+    model = create_model(model_name)
+    dataset = SIPDataSet(features, labeled_bc_dir)
+    start_time = datetime.now()
+    training_res = model.train(dataset)
+    elapsed = datetime.now() - start_time
+    write_json({
+        'datetime': datetime.now().isoformat(),
+        'labeled_bc_dir': str(labeled_bc_dir),
+        'model_name': model_name,
+        'features': features,
+        'training_time_seconds': elapsed.total_seconds(),
+        'training_res': training_res
+    }, training_results_path)
 
 
 def main():
@@ -24,12 +61,8 @@ def main():
     labeled_bc_dir = pathlib.Path(args.labeled_bc_dir)
     model_name = args.model
     features = args.use_features
-    print(labeled_bc_dir, model_name, features)
-
-    model = GraphSageSIPLocalizer()
-    dataset = SIPDataSet(features, labeled_bc_dir)
-    res = model.train(dataset)
-    print(res)
+    training_results_path = pathlib.Path(args.save_results_path)
+    run(labeled_bc_dir, model_name, features, training_results_path)
 
 
 if __name__ == '__main__':
