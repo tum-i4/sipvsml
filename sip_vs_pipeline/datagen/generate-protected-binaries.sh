d37c49d637b268572404e5a80f9ea2d3597151c3
Nika <ndogh13@freeuni.edu.ge>
2021-07-05 05:08:19 +0400

    Include svf features in data generation script

--- sip_vs_pipeline/datagen/generate-protected-binaries.sh
+++ sip_vs_pipeline/datagen/generate-protected-binaries.sh
@@ -88,7 +88,19 @@ function generateSimple {
 	verifyCountFiles "$GENERATIONPATH/$DATASET/*" "160"
 }
 
-
-
 generateSimple
 generateMibench
+
+function checkoutput {
+        if [ $? -ne 0 ]; then
+                echo $1
+                exit 1
+        fi
+}
+for ds in 'simple-cov' 'mibench-cov'; do
+        waitforjobs $(nproc)
+        echo SPAWNING $(nproc) processes to generate CSV files from labled BC samples
+        bash ../program-dependence-graph/collect-seg-dataset-features.sh LABELED-BCs/$ds skip > /dev/null &
+done
+waitforjobs 1
+checkoutput 'Failed to generate CSV files'
