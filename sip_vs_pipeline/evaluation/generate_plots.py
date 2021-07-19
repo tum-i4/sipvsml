9d945254fd1597b7d582505f6f3e240891f56e2b
Nika <ndogh13@freeuni.edu.ge>
2021-07-19 05:11:18 +0400

    Add initial plot generation scripts in evaluation stage

---
+++ sip_vs_pipeline/evaluation/generate_plots.py
@@ -0,0 +1,47 @@
+import argparse
+import json
+import pathlib
+
+import pandas as pd
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate plots from model training results binaries')
+    parser.add_argument('labeled_bc_dir', help='Directory where labeled binaries are stored')
+    args = parser.parse_args()
+    return args
+
+
+def find_results_json_files(path):
+    if path.is_file():
+        if path.name.endswith('results.json'):
+            with open(path) as inp:
+                yield json.load(inp)
+    else:
+        for child in path.iterdir():
+            yield from find_results_json_files(child)
+
+
+def reconcile_results_into_dataframe(results_list):
+    data = []
+    for res_data in results_list:
+        data.append({
+            'data_source': res_data['data_source'],
+            'obfuscation': res_data['data_dir'],
+            'features': ' + '.join(res_data['features']),
+            **res_data['results']['classifier']
+        })
+    return pd.DataFrame(data)
+
+
+def main():
+    args = parse_args()
+    labeled_bc_dir = pathlib.Path(args.labeled_bc_dir)
+    result_files = find_results_json_files(labeled_bc_dir)
+    res = list(result_files)
+    df = reconcile_results_into_dataframe(res)
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
