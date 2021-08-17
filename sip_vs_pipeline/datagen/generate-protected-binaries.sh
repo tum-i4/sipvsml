b433d2651b60d5f9455543e140f19ccbff435a17
Nika <ndogh13@freeuni.edu.ge>
2021-08-17 20:16:37 +0400

    Unite raw dataset generation in a single script

--- sip_vs_pipeline/datagen/generate-protected-binaries.sh
+++ sip_vs_pipeline/datagen/generate-protected-binaries.sh
@@ -1,11 +1,8 @@
-# File taken from https://github.com/mr-ma/composition-sip-eval/blob/smwyg/generate-ml-files.sh
-
 FLA='FLAs'
 SUB='SUB'
 BCF='BCF'
 
 PERC=30
-DATASET='simple-cov2'
 GENERATIONPATH='LABELED-BCs/'
 function edit {
 	echo switched to $1
@@ -21,25 +18,20 @@ function waitforjobs {
     while test $(jobs -p | wc -w) -ge "$1"; do wait -n; done
 }
 function generate {
-
 	for ds in $DATASET;
 	do
 		for prot in 'OH' 'CFI' 'SC' ;
-			#
 		do
-			#N=16
-			#(
 			for combination in "$@";
 			do
 				#generator-prot-obf.sh OH FLAs-SUB-BCF coverage mibench-OH-FLAs-SUB-BCF
-			#	((i=i%N)); ((i++==0)) && wait
 				comb_dir="${combination/FLAs/FLA}"
 				comb_dir="${comb_dir/BCF/BCF$PERC}"
 				waitforjobs $(nproc)
-				echo generator-prot-obf.sh $prot $combination "simple-cov" "$GENERATIONPATH/$ds/$comb_dir"
-				bash generator-prot-obf.sh $prot $combination "simple-cov" "$GENERATIONPATH/$ds/$comb_dir" > /dev/null &
+				echo generator-prot-obf.sh $prot $combination $ds "$GENERATIONPATH/$OUT_DIR/$comb_dir"
+				bash generator-prot-obf.sh $prot $combination $ds "$GENERATIONPATH/$OUT_DIR/$comb_dir" > /dev/null &
 			done
-			waitforjobs 1
+			waitforjobs 1  
 		done
 	done
 
@@ -49,23 +41,24 @@ function verifyCountFiles {
 	VerifyPath=$1
 	BCInFolder=$2
 	for folder in ${VerifyPath};
-	do
+	do 
 		count=$(ls -1 $folder/*.bc|wc -l)
 		if [ $count != $2 ]; then
 			echo "COUNTERR"
-			echo "$folder/*.bc does not have $2 items but $count items"
+			echo "$folder/*.bc does not have $2 items but $count items" 
 			#exit 1
 		fi
 
 	done
 }
 
-function generateSimple2 {
-	DATASET='simple-cov2'
+function generateMibench {
+	DATASET='mibench-cov'
+  OUT_DIR='mibench-cov'
 
 	generate "NONE"
 	edit 30
-	generate "$BCF" "$FLA-$BCF" "$BCF-$FLA" "$SUB-$BCF" "$BCF-$SUB" "$FLA-$BCF-$SUB" "$FLA-$SUB-$BCF" "$BCF-$FLA-$SUB" "$BCF-$SUB-$FLA" "$SUB-$FLA-$BCF" "$SUB-$BCF-$FLA" "$SUB" "$FLA" "$SUB-$FLA" "$FLA-$SUB" "$BCF-$FLA"2 "$BCF-$FLA"2"-$SUB"2 "$BCF-$SUB"2"-$FLA"2
+	generate "$BCF" "$FLA-$BCF" "$BCF-$FLA" "$SUB-$BCF" "$BCF-$SUB" "$FLA-$BCF-$SUB" "$FLA-$SUB-$BCF" "$BCF-$FLA-$SUB" "$BCF-$SUB-$FLA" "$SUB-$FLA-$BCF" "$SUB-$BCF-$FLA" "$SUB" "$FLA" "$SUB-$FLA" "$FLA-$SUB" "$BCF-$FLA"2 "$BCF-$FLA"2"-$SUB"2 "$BCF-$SUB"2"-$FLA"2 
 
 	verifyCountFiles "$GENERATIONPATH/$DATASET/*" "76"
 
@@ -76,6 +69,38 @@ function generateSimple2 {
 	edit 100
 	generate "$BCF" "$BCF-$FLA" "$BCF-$FLA-$SUB" "$BCF-$SUB" "$BCF-$SUB-$FLA" "$FLA-$BCF" "$FLA-$BCF-$SUB" "$FLA-$SUB-$BCF" "$SUB-$BCF" "$SUB-$BCF-$FLA" "$SUB-$FLA-$BCF"
 	verifyCountFiles "$GENERATIONPATH/$DATASET/*" "76"
+
 }
 
+function generateSimple {
+	DATASET='simple-cov'
+	OUT_DIR='simple-cov'
+
+	edit 30
+	generate "NONE"
+	generate "$BCF" "$FLA-$BCF" "$BCF-$FLA" "$SUB-$BCF" "$BCF-$SUB" "$FLA-$BCF-$SUB" "$FLA-$SUB-$BCF" "$BCF-$FLA-$SUB" "$BCF-$SUB-$FLA" "$SUB-$FLA-$BCF" "$SUB-$BCF-$FLA" "$SUB" "$FLA" "$SUB-$FLA" "$FLA-$SUB"
+	verifyCountFiles "$GENERATIONPATH/$OUT_DIR/*" "160"
+}
+
+function generateSimple2 {
+	DATASET='simple-cov'
+	OUT_DIR='simple-cov2'
+
+	generate "NONE"
+	edit 30
+	generate "$BCF" "$FLA-$BCF" "$BCF-$FLA" "$SUB-$BCF" "$BCF-$SUB" "$FLA-$BCF-$SUB" "$FLA-$SUB-$BCF" "$BCF-$FLA-$SUB" "$BCF-$SUB-$FLA" "$SUB-$FLA-$BCF" "$SUB-$BCF-$FLA" "$SUB" "$FLA" "$SUB-$FLA" "$FLA-$SUB" "$BCF-$FLA"2 "$BCF-$FLA"2"-$SUB"2 "$BCF-$SUB"2"-$FLA"2
+
+	verifyCountFiles "$GENERATIONPATH/$OUT_DIR/*" "76"
+
+	#edit bogus contrl flow to 40
+	edit 40
+	generate "$BCF" "$BCF-$FLA" "$BCF-$FLA-$SUB" "$BCF-$SUB" "$BCF-$SUB-$FLA" "$FLA-$BCF" "$FLA-$BCF-$SUB" "$FLA-$SUB-$BCF" "$SUB-$BCF" "$SUB-$BCF-$FLA" "$SUB-$FLA-$BCF"
+	verifyCountFiles "$GENERATIONPATH/$OUT_DIR/*" "76"
+	edit 100
+	generate "$BCF" "$BCF-$FLA" "$BCF-$FLA-$SUB" "$BCF-$SUB" "$BCF-$SUB-$FLA" "$FLA-$BCF" "$FLA-$BCF-$SUB" "$FLA-$SUB-$BCF" "$SUB-$BCF" "$SUB-$BCF-$FLA" "$SUB-$FLA-$BCF"
+	verifyCountFiles "$GENERATIONPATH/$OUT_DIR/*" "76"
+}
+
+generateSimple
+generateMibench
 generateSimple2
--- sip_vs_pipeline/datagen/generate_dataset.py
+++ sip_vs_pipeline/datagen/generate_dataset.py
@@ -34,19 +34,15 @@ def build_docker_image():
     print(f'Building the docker image - {IMAGE_NAME}...DONE')
 
 
-def read_generate_ml_files():
-    with open(GEN_PROTECTED_BINARIES_SH_PATH) as inp:
-        return inp.read()
-
-
 def run_data_generation_script(out_dir):
     print('Running data generation in docker container...')
     docker_run = f'docker run --rm ' \
                  f'-v "{out_dir}":/home/sip/paperback:rw ' \
+                 f'-v "{GEN_PROTECTED_BINARIES_SH_PATH}":/home/sip/eval/{GEN_PROTECTED_BINARIES_SH_PATH.name} ' \
                  f'--security-opt seccomp=unconfined {IMAGE_NAME}'
     data_script = 'mkdir -p /home/sip/paperback/LABELED-BCs && ' \
                   'ln -s /home/sip/paperback/LABELED-BCs /home/sip/eval/LABELED-BCs && ' \
-                  'bash generate-ml-files.sh'
+                  f'bash {GEN_PROTECTED_BINARIES_SH_PATH.name}'
     subprocess.run(f'{docker_run} bash -c "{data_script}"', shell=True, check=True)
     print('Running data generation in docker container...DONE')
 
